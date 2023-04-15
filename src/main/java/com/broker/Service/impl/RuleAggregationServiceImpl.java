29f7489f56a9bee35b14d816c85d93e1ff344877
bailing jiang <bailing.jiang@svfactory.com.cn>
2023-04-15 17:53:30 +0800

    test

--- src/main/java/com/broker/Service/impl/RuleAggregationServiceImpl.java
+++ src/main/java/com/broker/Service/impl/RuleAggregationServiceImpl.java
@@ -4,6 +4,7 @@ import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 
 import com.broker.Entity.RuleAggregation;
 import com.broker.Mapper.RuleAggregationMapper;
+import com.broker.Service.RuleAggregationService;
 
 
 public class RuleAggregationServiceImpl extends ServiceImpl<RuleAggregationMapper, RuleAggregation> implements RuleAggregationService {
